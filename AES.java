24b9786099930a9933119fea2536ae88c8bb9a1e
Carl Yarwood <cdy8858@truman.edu>
2018-11-20 15:12:33 -0600

    added scafulding to AES

---
+++ AES.java
@@ -0,0 +1,22 @@
+
+
+public class AES(){
+    private byte[] key;
+    private byte[] message;
+    public AES(){
+
+    }
+    public void setKey(String key){
+	this.key = key;
+    }
+    public void setMessage(String message){
+	this.message = message;
+    }
+    public byte[] encrypt(){
+
+    }
+    public byte[] decrypte(){
+
+    }
+
+}
