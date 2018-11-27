df46b5a7d125e8c607ed6e2500d49d180bad81c5
Carl Yarwood <cdy8858@vh222006.truman.edu>
2018-11-27 16:26:33 -0600

    added run time port chooser

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 public class Main{
 
     public static void main(String[] args){
-	TCPServer server = new TCPServer(3389);
+	Scanner scan = new Scanner(System.in);
+	System.out.print("Enter the port you would like your server on: ");
+	int port = scan.nextInt();
+	TCPServer server = new TCPServer(port);
 	server.createSocket();
 	while(true){
 	    server.listenForConnection();
